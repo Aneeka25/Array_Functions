dbc2adb59eabf11513ad91938d60bd1a3b81a845
Aneeka25 <154527297+Aneeka25@users.noreply.github.com>
2025-02-03 23:03:26 +0530

    separate Even Odd

--- ArrayLists.java
+++ ArrayLists.java
@@ -19,41 +19,4 @@ public class ArrayLists {
         System.out.println("Odd numbers: " + odd);
     }
 
-    public static int findSmallestDistance(int[] numbers) {
-        if (numbers.length < 2)
-            return -1;
-
-        int minIndex = 0;
-        int minDistance = Math.abs(numbers[1] - numbers[0]);
-
-        for (int i = 1; i < numbers.length - 1; i++) {
-            int distance = Math.abs(numbers[i + 1] - numbers[i]);
-            if (distance < minDistance) {
-                minDistance = distance;
-                minIndex = i;
-            }
-        }
-        return minIndex;
-    }
-
-    public static ArrayList<Integer> arrayToList(int[] array) {
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int num : array) {
-            list.add(num);
-        }
-        // System.out.println("Converted to ArrayList: " + List + " (Type: " +
-        // list.getClass().getSimpleName() + ")");
-        System.out.println("Data type of 'list': " + list.getClass().getName());
-        return list;
-    }
-
-    public static int[] listToArray(ArrayList<Integer> list) {
-        int[] array = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            array[i] = list.get(i);
-        }
-        System.out.println("Data type of 'array': " + array.getClass().getName());
-        return array;
-    }
-
-}
+    
